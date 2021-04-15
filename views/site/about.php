73a4028f62831d17a913b1083a7bc38a8e03ad53
Exence <81925668+Exence@users.noreply.github.com>
2021-04-16 02:53:38 +0300

    Add files via upload

---
+++ views/site/about.php
@@ -0,0 +1,17 @@
+<?php include ROOT . '/views/layouts/header.php'; ?>
+
+<section>
+    <div class="container">
+        <div class="row">
+
+            <div class="col-lg-6">
+                <h4>Информация о магазине</h4>
+
+                <br/>
+                <p>разработка интернет-магазина в рамках РГЗ по Web-программированию </p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php include ROOT . '/views/layouts/footer.php'; ?>
